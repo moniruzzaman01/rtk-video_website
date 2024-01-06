f208e2ae28b21e618adb37e2d64ce5686b0cd7d2
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2024-01-06 16:32:09 +0600

    template divided into components and project restructured

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import searchSvg from "../assets/images/search.svg";
+import logo from "../assets/images/lws.svg";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-slate-100 shadow-md">
+      <div className="max-w-7xl mx-auto px-5 lg:px-0 flex justify-between py-3">
+        <a href="/">
+          <img className="h-10" src={logo} alt="Learn with Sumit" />
+        </a>
+        <div className="border border-slate-200 flex items-center bg-white h-10 px-5 rounded-lg text-sm ring-emerald-200">
+          <form>
+            <input
+              className="outline-none border-none mr-2"
+              type="search"
+              name="search"
+              placeholder="Search"
+            />
+          </form>
+          <img
+            className="inline h-4 cursor-pointer"
+            src={searchSvg}
+            alt="Search"
+          />
+        </div>
+      </div>
+    </nav>
+  );
+}
